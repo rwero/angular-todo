251796fb6424cb47a93c9a3771e4204caf017036
rwero <redgit115@protonmail.com>
2022-09-15 08:45:30 +0100

    created todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+
+  showDate():String {
+	return `${new Date().toUTCString().split("GMT")[0]}`
+  }
+  items = ["learn angular", "build todo app","deploy it"];
 }
